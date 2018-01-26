b487cfe73d2235c4ea82da1fa85aeff15f2e9690
Werick Vieira <werick.vieira@yahoo.com.br>
2018-01-26 20:40:50 -0200

    colocando o eslint no padrao do airbnb

--- app/src/js/random.js
+++ app/src/js/random.js
@@ -1,91 +1,92 @@
 let counter = 0;
 const newData = [];
 
-const randomItens = (itens,	quantity) => {
-	const amount = itens.length;
-	const arritens = [];
-	while (arritens.length < quantity) {
-		const curr = itens[Math.floor((Math.random() * amount))];
-		if (arritens.indexOf(curr) > -1) {
-			continue;
-		}
-		arritens[arritens.length] = curr;
-	}
-	return arritens;
+const randomItens = (itens, quantity) => {
+  const amount = itens.length;
+  const arritens = [];
+  while (arritens.length < quantity) {
+    const curr = itens[Math.floor((Math.random() * amount))];
+    if (arritens.indexOf(curr) <= -1) {
+      arritens[arritens.length] = curr;
+      // continue;
+    }
+    // arritens[arritens.length] = curr;
+  }
+  return arritens;
 };
 
 const randomAssociate = (currAssociate, itens) => {
-	const elements = randomItens(itens, 2);
-	const isCharacter = currAssociate.caracteristica.filter(i => { 
-		return elements.every(({ jogador, caracteristica }) => {
-			if (currAssociate.jogador === jogador) {
-				return false;
-			}
-			return caracteristica.indexOf(i) > -1;
-		});
-	});
+  const elements = randomItens(itens, 2);
+  const isCharacter = currAssociate.caracteristica.filter(i => { 
+    return elements.every(({ jogador, caracteristica }) => {
+      if (currAssociate.jogador === jogador) {
+        return false;
+      }
+      return caracteristica.indexOf(i) > -1;
+    });
+  });
 
-	if (counter >= 1000) {
-		counter = 0;
-		return elements;
-	} else if (isCharacter.length <= 0) {
-		++counter;
-		return false;
-	} else {
-		counter = 0;
-		return elements;
-	}
+  if (counter >= 1000) {
+    counter = 0;
+    return elements;
+  } else if (isCharacter.length <= 0) {
+    ++counter;
+    return false;
+  } else {
+    counter = 0;
+    return elements;
+  }
 };
 
 const findAssociate = (curr, itens, obj) => {
-	const value = randomAssociate(curr, itens);
-	if (!value) {
-		findAssociate(curr, itens, obj);
-	} else {
-		obj[curr.correctAnswer] = [];
-		obj[curr.correctAnswer].push(shuffle(value.concat(curr)));
-	}
+  const value = randomAssociate(curr, itens);
+  if (!value) {
+    findAssociate(curr, itens, obj);
+  } else {
+    obj[curr.correctAnswer] = [];
+    obj[curr.correctAnswer].push(shuffle(value.concat(curr)));
+  }
 };
 
 const mountAssociateItens = (selectedItens, itens) => (
-	selectedItens.reduce((prev, curr, i) => {
-		const id = Math.round(Date.now() * (i+1) / 1000);
-		curr['correctAnswer'] = id;
-		findAssociate(curr, itens, prev);
-		return prev;
-	}, {})
+  selectedItens.reduce((prev, curr, i) => {
+    const id = Math.round(Date.now() * (i+1) / 1000);
+    curr['correctAnswer'] = id;
+    findAssociate(curr, itens, prev);
+    return prev;
+  }, {})
 );
 
 const controlRandom = (data) => {
-	newData.push(mountAssociateItens(randomItens(data, 10), data));
+  newData.push(mountAssociateItens(randomItens(data, 10), data));
 };
 
 const initRandom = (data) => {
-	controlRandom(modifyData(data));
-	return newData;
+  controlRandom(modifyData(data));
+  return newData;
 };
 
 const getDataRandom = () => (
-	newData
+  newData
 );
 
 // Funcoes abaixo vão para outro módulo
 const modifyData = (itens) => (
-	itens.map((e) => {
-		e['caracteristica'] = e['caracteristica'].split(",").map(removeSpace);
-		return e;
-	})
+  itens.map((e) => {
+    e['caracteristica'] = e['caracteristica'].split(",").map(removeSpace);
+    return e;
+  })
 );
 
 const removeSpace = s => s.replace(/\s+/, "");
 
 const shuffle = arr => (
-	arr.sort( () => .5 - Math.random() )
+  arr.sort( () => .5 - Math.random() )
 );
 
 export {
-	initRandom as default,
-	getDataRandom
+  initRandom as default,
+  getDataRandom
 };
 
 
--- app/src/js/view.js
+++ app/src/js/view.js
@@ -10,58 +10,61 @@
 //   });
 // };
 
-const allItens = document.querySelectorAll(".g-block__item"); 
+const allItens = document.querySelectorAll('.g-block__item');
 const isArr = [];
 
-const randomBoxIntro = (itens, isArr) => {
+const randomBoxIntro = (itens) => {
   let isDiff = false;
   const range = 5; // De acordo com dispositivo
   const colors = ['yellow', 'lilac', 'blue', 'orange', 'green'];
   while (!isDiff) {
-    const randomItem = itens[Math.floor((Math.random() * itens.length ))];
-    const randomColor = colors[Math.floor((Math.random() * colors.length ))];
-    const currColor = randomItem.getAttribute("color");
+    const randomItem = itens[Math.floor((Math.random() * itens.length))];
+    const randomColor = colors[Math.floor((Math.random() * colors.length))];
+    const currColor = randomItem.getAttribute('color');
     const index = [...itens].indexOf(randomItem);
     const itemUp = itens[index - range];
     const itemDown = itens[index + range];
     const itemRight = itens[index + 1];
     const itemLeft = itens[index - 1];
-    const mapSiblings = [itemUp, itemDown, itemRight, itemLeft].map(item => 
-      item !== undefined ? item.getAttribute("color") : null 
-    );
-    if (currColor === randomColor 
+    const mapSiblings = [itemUp, itemDown, itemRight, itemLeft].map(item => (
+      item !== undefined ? item.getAttribute('color') : null
+    ));
+
+    if (currColor === randomColor
       || mapSiblings.indexOf(randomColor) > -1
       || isArr.indexOf(randomItem) > -1) {
-      continue;
+      // continue;
+      isDiff = false;
+    } else {
+      isArr.push(randomItem);
+      isDiff = true;
+      randomItem.setAttribute('color', randomColor);
     }
-    isArr.push(randomItem);
-    isDiff = true;
-    randomItem.setAttribute("color", randomColor);
-  };
+  }
 };
 
 const controlRandomBoxIntro = () => {
-  randomBoxIntro(allItens, isArr);
-  setTimeout(initIntro, 1000);
-  // window.requestAnimationFrame(initIntro)
+  randomBoxIntro(allItens);
+  // setTimeout(initIntro, 1000);
+  window.requestAnimationFrame(initIntro);
 };
 
 const initIntro = () => {
   if (isArr.length <= 4) {
-    // setTimeout(controlRandomBoxIntro, 1000);
-    window.requestAnimationFrame(controlRandomBoxIntro)
+    setTimeout(controlRandomBoxIntro, 3000);
+    // window.requestAnimationFrame(controlRandomBoxIntro);
   }
 };
 
 const initView = (data) => {
   initIntro();
   // isMobile(800) ? setHeightMobile() : null;
-  // console.log("data", data[0])
+  console.log('data', data[0]);
   // data.forEach((item) => {
   //  console.log("item", item);
   // });
 };
 
 export {
-  initView as default
+  initView as default,
 };
